6524ab5f92bf0cbe5720388d1ed3bee11848b38f
Glen Pink <glen.pink@gmail.com>
2014-07-07 22:22:15 +1000

    Added json bow to human readable format.

--- src/schwa-py-lsh/hashes.py
+++ src/schwa-py-lsh/hashes.py
@@ -15,7 +15,6 @@ class Projection:
         return BitArray(h)
 
 
-
 def main(n_vecs):
    generator = TestVectorGenerator()
    proj = Projection(100, 1000)
@@ -24,6 +23,7 @@ def main(n_vecs):
        signature = proj.hash(vec)
        print(id, vec)
        print(signature.bin)
+
 
 def test_random_vecs(n_vecs):
    generator = TestVectorGenerator()
@@ -41,6 +41,31 @@ def test_random_vecs(n_vecs):
        print(signature == signature2)
        print(len((signature ^ signature2).bin.replace('0', '')))
 
+import json
+def test_json():
+    BITS = 128
+
+    f_space = set()
+    docs = []
+    for id, name, bow in json.load(open('test/wiki_data.json')):
+        docs.append((id, name, bow))
+        f_space |= set(bow.keys())
+
+    f_space = filter(lambda x: x, f_space) # remove empty strings
+    # vectors are sparse so we want to lookup into them directly
+    f_space = dict(((v, k) for k, v in enumerate(f_space)))
+
+    length = len(f_space.keys())
+
+    proj = Projection(BITS, length)
+    for id, name, bow in docs:
+        vec = [0 for x in range(length)]
+        for word, count in bow.items():
+            if not word: # remove empty strings, again
+                continue
+            vec[f_space[word]] = count
+        print(id, proj.hash(vec).bin)
 
 if __name__ == '__main__':
-    main(int(sys.argv[1]))
+    #main(int(sys.argv[1]))
+    test_json()
