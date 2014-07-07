59d957d0d9faec873c535da5f935d4d073785d29
Dominick Ng <dominick8n@gmail.com>
2014-07-07 13:40:15 +1000

    first commit of model and reader; adding representation of reading from filelike to output in human readable format

---
+++ src/io/model.py
@@ -0,0 +1,14 @@
+import struct
+
+class LSHItem(object):
+    def __init__(self, id, signature, count=0, vector=None):
+        self.id = id
+        self.signature = signature
+        self.count = count
+        if vector is None:
+            self.vector = []
+        else:
+            self.vector = vector
+
+    def __str__(self):
+        return '{} {}'.format(self.id, self.signature)
