be10f6fd0400c2317e11c375d277e58d84a3c8fa
Glen Pink <glen.pink@gmail.com>
2014-07-08 16:44:42 +1000

    Added setup.py and refactored directory structure.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim: set ts=4 sw=4 et ai:
+
+from setuptools import setup, find_packages
+import os
+VERSION = '0.1'
+
+setup(
+    name='lsh',
+    version = VERSION,
+    description = 'lsh',
+    url = 'https://github.com/schwa-lab/lsh',
+    packages = ['src'],
+    install_requires = [
+    ],
+)
