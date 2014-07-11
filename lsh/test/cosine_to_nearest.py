6c4a9415e521c9e89655bdb16c3e13edb297a369
Glen Pink <glen.pink@gmail.com>
2014-07-11 10:59:59 +1000

    Added test set of size 500.

--- lsh/test/cosine_to_nearest.py
+++ lsh/test/cosine_to_nearest.py
@@ -4,7 +4,7 @@ import sys
 import math
 from collections import defaultdict
 
-def main(filename):
+def main(filename, neigh=10):
     pairs = defaultdict(list)
     for line in open(filename):
         id1, id2, cosine = line.strip().split(' ')
@@ -14,7 +14,7 @@ def main(filename):
  
     for id, neighbours in pairs.items():
         # get 10 nearest neighbours and print
-        print('{}\t{}'.format(id, '\t'.join(('{} {}'.format(x[0], x[1]) for x in sorted(neighbours, key=lambda x: x[1], reverse=True)[:10]))))
+        print('{}\t{}'.format(id, '\t'.join(('{} {}'.format(x[0], x[1]) for x in sorted(neighbours, key=lambda x: x[1], reverse=True)[:neigh]))))
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1], int(sys.argv[2])) # cosine file, number of neighbours
