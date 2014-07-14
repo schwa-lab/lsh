c1d0524a6367ca877e06be61f890e227e9e33b3f
Dominick Ng <dominick8n@gmail.com>
2014-07-14 13:50:43 +1000

    * more testing code

--- lsh/test_bits.py
+++ lsh/test_bits.py
@@ -1,13 +1,17 @@
+import random
 import pyximport; pyximport.install()
 from lsh import bits
 
 
 if __name__ == "__main__":
-    h = bits.Hash([True if i < 3 else False for i in range(64)])
+    h = bits.Hash([True if i < 3 else False for i in range(128)])
+    print(bits.NBITS())
     h.print_data()
     h.print_working()
     print(bin(h.get_prefix(5)))
-    h.lrotate(1)
+    h.lrotate(65)
     h.print_data()
     h.print_working()
     print(bin(h.get_prefix(5)))
+    h.shuffle([random.randint(i, 127) for i in range(128)])
+    h.print_working()
